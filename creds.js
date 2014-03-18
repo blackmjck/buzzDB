6b8ec948329b18579c7907990619c8a3102103d9
Steven Wiggins <steven_wiggins@bitbucket.org>
2014-03-17 23:23:11 -0400

    Updating sample connection and instructions

--- creds.js
+++ creds.js
@@ -1,6 +1,11 @@
 /**
  * Semi-securely stores the MongoDB connection information
+ *
+ * Fill in your own MongoDB connection here, in the format:
+ * "mongodb://[username[:password]@]domain[,domain,domain,...]:port_number[/database_name]"
+ *
+ * The minimum required would be "mongodb://localhost"
  */
 
 // Sample version:
-// exports.connect_url = "mongodb://localhost";
+// exports.connect_url = "mongodb://localhost/buzz";
